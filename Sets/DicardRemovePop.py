8572649428425e43e991d3178565c2d9030f9f57
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2019-07-24 20:04:15 -0700

    Create DicardRemovePop.py

---
+++ Sets/DicardRemovePop.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem
+
+if __name__ == "__main__":
+    n = int(input())
+    s = set(map(int, input().split()))
+    N = int(input())
+
+    for _ in range(N):
+        method, *args = input().split()
+        getattr(s, method)(*map(int, args))
+
+    print(sum(s))
